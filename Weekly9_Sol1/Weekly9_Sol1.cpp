a0713ef00ba558058fe4c66f45d8b68d88f291a3
RACHA TUNGTRAKOOL <65010902@KMITL.AC.TH>
2022-10-16 21:08:13 +0700

    add _CRT_SECURE

--- Weekly9_Sol1/Weekly9_Sol1.cpp
+++ Weekly9_Sol1/Weekly9_Sol1.cpp
@@ -1,10 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 int main()						//Problem 110, Solution 1, ID Student 65010902
 {
 	int person;
 	int card = 0;
 	printf("Enter number of person: ");
-	scanf_s("%d", &person);
+	scanf("%d", &person);
 
 	for (int i = 1; i <= person; i++)		// count each person
 	{
